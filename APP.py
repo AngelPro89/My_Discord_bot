2ff20179a145494a297f887ba9797c58e3ba4cb1
AngelPro89 <Angelf.zavala.lara@gmail.com>
2025-09-19 23:15:53 -0600

    API INSTALLED

--- APP.py
+++ APP.py
@@ -1,4 +1,4 @@
-import random, string
+import random, string,discord, os,requests
 def pswd_gnrtor(lenght= 25):
     charac_pswrd = string.ascii_letters + string.digits + string.punctuation +string.ascii_uppercase
     pswd = ""
@@ -12,4 +12,19 @@ def flip_coin():
         return "HEADS"
     else:
         return "TAILS"
+def meme ():
     
+    with open("img/meme 2.jpeg" ,"rb") as f:
+        picture = discord.File(f)
+        return picture
+def memes ():
+    
+    lista_memes = random.choice(os.listdir("img"))
+    with open(f"img/{lista_memes}" ,"rb") as f:
+        picture = discord.File(f)
+        return picture
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
--- BOT.py
+++ BOT.py
@@ -1,20 +1,121 @@
-import discord,os
-import APP
-from dotenv import load_dotenv
+import discord
+import os, APP
+import asyncio
+import yt_dlp as youtube_dl
 from discord.ext import commands
+from dotenv import load_dotenv
 
+# Configuración para youtube-dl y ffmpeg
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0', 
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+ytdl = ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+# Clase para la fuente de audio de YouTube
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            data = data['entries'][0]
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+# Clase Music como Cog
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+        await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, query):
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+        await ctx.send(f'Now playing: {query}')
+
+    @commands.command()
+    async def yt(self, ctx, *, url):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    @yt.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+# Carga la variable de entorno
 load_dotenv()
 token = os.getenv("dt")
 
+# Configuración del bot e Intents
 intents = discord.Intents.default()
 intents.message_content = True
-
+intents.voice_states = True 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+# Evento on_ready
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    await bot.add_cog(Music(bot))
 
+# Comandos existentes
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
@@ -23,9 +124,27 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+
 @bot.command(name= "pswd")
-async def pswds(ctx,lenght = 25):
-    data= APP.pswd_gnrtor(lenght)
+async def pswds(ctx, lenght=25):
+    data = APP.pswd_gnrtor(lenght)
     await ctx.send(f"🔒Contraseña generada: {data}")
 
+@bot.command(name = "meme")
+async def meme(ctx):
+    photo = APP.meme()
+    await ctx.send(file= photo)
+@bot.command(name = "memes")
+async def memes(ctx):
+    photo = APP.memes()
+    await ctx.send(file= photo)
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = APP.get_duck_image_url()
+    await ctx.send(image_url)
+
+# Ejecución del bot
 bot.run(token)
